e8d22bd44c3f17efce83e79a7f20bdc6356c7792
Olumide Oyegoke <59411943+olumide1128@users.noreply.github.com>
2023-01-03 18:54:34 +0000

    Added Validation Constraint for Fields

--- src/main/java/com/lumidii/tasktrackerapi/Entity/Task.java
+++ src/main/java/com/lumidii/tasktrackerapi/Entity/Task.java
@@ -1,21 +1,41 @@
 package com.lumidii.tasktrackerapi.Entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name= "TASK_TBL")
 public class Task {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	
+	@Column(name="TEXT")
 	private String text;
+	
+	@Column(name="`DAY`")
 	private String day;
+	
+	@Column(name="REMINDER")
 	private boolean reminder;
+
+	
+	//Constructor
+	public Task(String day, String text, boolean reminder) {
+		this.text = text;
+		this.day = day;
+		this.reminder = reminder;
+	}
+	
+	
 }
--- src/main/java/com/lumidii/tasktrackerapi/TaskTrackerApiApplication.java
+++ src/main/java/com/lumidii/tasktrackerapi/TaskTrackerApiApplication.java
@@ -1,13 +1,27 @@
 package com.lumidii.tasktrackerapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.lumidii.tasktrackerapi.Entity.Task;
+import com.lumidii.tasktrackerapi.repository.TaskRepository;
 
 @SpringBootApplication
 public class TaskTrackerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskTrackerApiApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner init(TaskRepository taskRepo) {
+		return args -> {
+			taskRepo.save(new Task("9th, January, 2023", "Visit Therapy", false));
+			taskRepo.save(new Task("26th, March, 2023", "Workout", true));
+			taskRepo.save(new Task("2nd, January, 2023", "Do Laundry", false));
+		};
 	}
 
 }
